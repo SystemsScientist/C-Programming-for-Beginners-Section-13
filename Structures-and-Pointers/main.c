d999a66613ae7f890cf0366d7bcfb1507b9780b1
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-04-03 05:59:03 -0500

    Adding Structures-and-Pointers files and directories

---
+++ Structures-and-Pointers/main.c
@@ -0,0 +1,32 @@
+/*
+    Author: Matt Johnson
+    Subject: pointers
+    Purpose:
+    Date: 04/03/2023
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct date {
+
+    int month;
+    int day;
+    int year;
+};
+
+int main(void) {
+
+    struct date today, *date_ptr;
+
+    date_ptr = &today;
+
+    date_ptr -> month = 4;
+    date_ptr -> day = 3;
+    date_ptr -> year = 2023;
+
+    printf("Today's date is %i/%i/%.2i.\n",
+        date_ptr -> month, date_ptr -> day, date_ptr -> year);
+
+    return 0;
+}
